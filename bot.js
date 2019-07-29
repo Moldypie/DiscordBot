0c73682303b5145f5eca1f7641df115d82b4eb20
Moldypie <48046412+Moldypie@users.noreply.github.com>
2019-07-29 09:08:35 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,78 @@
+const config = require("./botconfig.json");
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true});
+var minutes = 1000 * 60;
+var hours = minutes * 60;
+var days = hours * 24;
+var years = days * 365;
+var d = new Date();
+var t = d.getTime();
+var y = Math.round(t / years); 
+var h = d.getHours();
+var grafica = 0;
+
+client.on("ready", async message => {
+	console.log("Online");
+	console.log(h);
+	console.log(t);
+	client.user.setActivity("Browsing Art"); 
+	setInterval(function() {
+		grafica();
+	}, 60 * 1000 * 60 * 24);
+});
+
+client.on("message", async message => {
+	if(message.author.bot) return;
+	if(message.content.indexOf(config.prefix) !== 0) return;
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === "image") {
+		!grafica();
+		grafica = 0;
+    }
+});
+//original command with no file logging
+/*function grafica(){
+	client.channels.get('584079072716521472').bulkDelete(100)
+		 .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+
+    client.channels.get('584079072716521472').send('Daily Grafica', {
+        files: [
+            "./Grafica/"+Math.floor((Math.random() * 197)+1)+".png"
+        ]
+    });
+}*/
+
+//logs grafica values to avoid repeats
+function grafica(){
+	client.channels.get('584079072716521472').bulkDelete(300)
+		 .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+	grafica = Math.floor((Math.random() * 197)+1);
+	!getValue(grafica);
+	client.channels.get('5840790727165214720').send('Daily Grafica', {
+		files: [
+			"./Grafica/"+grafica+".png"
+		]
+	});
+}
+
+//probably a shit way of reading/writing/randomizing but fuck ittttttt
+function getValue(){
+	fs = require('fs')
+	var values = fs.readFileSync("./grafica.txt").toString('utf-8');
+	for (var i=0; i<values.length; i++) 
+	{
+        if (values[i].match(grafica))
+		{ 
+			grafica = Math.floor((Math.random() * 197)+1);
+			!getValue(grafica);
+		}
+    }
+	fs.appendFileSync('./grafica.txt', "\n"+grafica, (err) => {
+		if(err) throw err;
+	});
+	return grafica;
+}
+
+client.login(config.token);
